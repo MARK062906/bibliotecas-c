ad25cface9f35c65da18f1fa2e4fe9d60933e1e2
Marko Andres Arcila Zapata <markus.34189@gmail.com>
2026-05-04 14:00:29 -0500

    Update pasos.h

--- pasos.h
+++ pasos.h
@@ -9,6 +9,10 @@ typedef struct {
 
 size_t pasos_proposicion_equivalente(const char *expresion, Paso *buffer, size_t capacidad);
 size_t pasos_induccion_basica(const char *enunciado, Paso *buffer, size_t capacidad);
+size_t pasos_contradiccion(const char *proposicion, Paso *buffer, size_t capacidad);
+size_t pasos_demostracion_directa(const char *proposicion, Paso *buffer, size_t capacidad);
+size_t pasos_analisis_casos(const char *proposicion, Paso *buffer, size_t capacidad);
+size_t pasos_obtener_por_tema(int tema_id, Paso *buffer, size_t capacidad);
 void pasos_imprimir(const Paso *pasos, size_t total);
 
 #endif
