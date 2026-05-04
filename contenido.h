2f63fd7a1700cfb6221329378bc968a09afe6a6f
Marko Andres Arcila Zapata <markus.34189@gmail.com>
2026-05-04 13:47:07 -0500

    Add search and detail retrieval functions
    Add functions to search and retrieve content details by ID

--- contenido.h
+++ contenido.h
@@ -11,6 +11,9 @@ typedef struct {
 
 size_t contenido_total(void);
 const ContenidoItem *contenido_obtener(size_t indice);
+const ContenidoItem *contenido_buscar_por_id(int id);
+const char *contenido_obtener_titulo(int id);
+const char *contenido_obtener_descripcion(int id);
 void contenido_imprimir_todos(void);
 void contenido_imprimir_por_id(int id);
 
